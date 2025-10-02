dbac7d27633a966b65ecf9fe62b0d53e4826b0a2
SimbaMupfu <scmupfururirwa@gmail.com>
2025-10-02 03:34:12 +0200

    Create Customer repository

---
+++ accounts/src/main/java/inc/sims/hustles/accounts/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package inc.sims.hustles.accounts.repository;
+
+import inc.sims.hustles.accounts.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
