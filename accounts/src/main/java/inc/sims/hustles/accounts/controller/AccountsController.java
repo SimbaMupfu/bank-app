0817b44f3027dc285c68780e4d5763d22c973d68
SimbaMupfu <scmupfururirwa@gmail.com>
2025-10-02 17:12:59 +0200

    Add an endpoint to create an account.

--- accounts/src/main/java/inc/sims/hustles/accounts/controller/AccountsController.java
+++ accounts/src/main/java/inc/sims/hustles/accounts/controller/AccountsController.java
@@ -1,10 +1,24 @@
 package inc.sims.hustles.accounts.controller;
 
+import inc.sims.hustles.accounts.constants.AccountsConstants;
+import inc.sims.hustles.accounts.dto.CustomerDto;
+import inc.sims.hustles.accounts.dto.ResponseDto;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path="/api/v1", produces = {MediaType.APPLICATION_JSON_VALUE})
 public class AccountsController {
+
+    @PostMapping("/create_account")
+    public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto){
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
+    }
 }
