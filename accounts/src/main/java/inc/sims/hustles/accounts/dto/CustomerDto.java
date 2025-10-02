602796f03608d160c3ff53bf0320da210347be1b
SimbaMupfu <scmupfururirwa@gmail.com>
2025-10-02 03:51:24 +0200

    Cretae

---
+++ accounts/src/main/java/inc/sims/hustles/accounts/dto/CustomerDto.java
@@ -0,0 +1,11 @@
+package inc.sims.hustles.accounts.dto;
+
+import lombok.Data;
+
+@Data
+public class CustomerDto {
+
+    private String name;
+    private String email;
+    private String mobileNumber;
+}
