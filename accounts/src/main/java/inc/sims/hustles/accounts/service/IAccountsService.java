1d44f9e33681430248434bda24a35717add0a7a1
SimbaMupfu <scmupfururirwa@gmail.com>
2025-10-02 22:09:56 +0200

    Create an interface for Accounts Service

---
+++ accounts/src/main/java/inc/sims/hustles/accounts/service/IAccountsService.java
@@ -0,0 +1,8 @@
+package inc.sims.hustles.accounts.service;
+
+import inc.sims.hustles.accounts.dto.CustomerDto;
+
+public interface IAccountsService {
+
+    void createAccount(CustomerDto customerDto);
+}
