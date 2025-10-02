5e90bb248a93ed32958ff680b33866102ba15827
SimbaMupfu <scmupfururirwa@gmail.com>
2025-10-02 03:26:45 +0200

    Create Accounts Model

---
+++ accounts/src/main/java/inc/sims/hustles/accounts/model/Accounts.java
@@ -0,0 +1,24 @@
+package inc.sims.hustles.accounts.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@AllArgsConstructor @NoArgsConstructor @Getter @Setter @ToString
+public class Accounts extends BaseModel{
+
+    @Column(name = "customer_id")
+    private Long customerId;
+
+    @Column(name = "account_number")
+    @Id
+    private Long accountNumber;
+
+    @Column(name = "account_type")
+    private String accountType;
+
+    @Column(name = "branch_address")
+    private String branchAddress;
+}
