87a36fc4e10212718c34b0dd86adede1b095f42e
SimbaMupfu <scmupfururirwa@gmail.com>
2025-10-02 03:35:47 +0200

    Create Accounts repository

---
+++ accounts/src/main/java/inc/sims/hustles/accounts/repository/AccountsRepository.java
@@ -0,0 +1,9 @@
+package inc.sims.hustles.accounts.repository;
+
+import inc.sims.hustles.accounts.model.Accounts;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountsRepository extends JpaRepository<Accounts, Long> {
+}
