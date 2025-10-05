e2b0698d089bfdcc7f53c51d5109bb51b9eacca9
SimbaMupfu <scmupfururirwa@gmail.com>
2025-10-05 21:39:21 +0200

    find if customer with mobile number already exists.

--- accounts/src/main/java/inc/sims/hustles/accounts/repository/CustomerRepository.java
+++ accounts/src/main/java/inc/sims/hustles/accounts/repository/CustomerRepository.java
@@ -4,6 +4,10 @@ import inc.sims.hustles.accounts.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    Optional<Customer> findByMobileNumer(String mobileNumber);
 }
