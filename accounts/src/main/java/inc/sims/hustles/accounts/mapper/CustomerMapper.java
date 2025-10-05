539bce747ad30a123d3744e090e690c5bca9e91b
SimbaMupfu <scmupfururirwa@gmail.com>
2025-10-05 09:11:33 +0200

    Add mappers in relation to customer and customer dto

--- accounts/src/main/java/inc/sims/hustles/accounts/mapper/CustomerMapper.java
+++ accounts/src/main/java/inc/sims/hustles/accounts/mapper/CustomerMapper.java
@@ -1,4 +1,21 @@
 package inc.sims.hustles.accounts.mapper;
 
+import inc.sims.hustles.accounts.dto.CustomerDto;
+import inc.sims.hustles.accounts.model.Customer;
+
 public class CustomerMapper {
+
+    public static CustomerDto mapToCustomerDto(CustomerDto customerDto, Customer customer){
+        customerDto.setName(customer.getName());
+        customerDto.setEmail(customer.getEmail());
+        customerDto.setMobileNumber(customer.getMobileNumber());
+        return customerDto;
+    }
+
+    public static Customer mapToCustomer(Customer customer, CustomerDto customerDto){
+        customer.setName(customerDto.getName());
+        customer.setEmail(customerDto.getEmail());
+        customer.setMobileNumber(customerDto.getMobileNumber());
+        return customer;
+    }
 }
